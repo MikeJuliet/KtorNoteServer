3dd4c0d93824811eedb49d339c105f77dea19602
MJ <marthinusjacobs@gmail.com>
2021-03-03 18:21:49 +0200

    The concept behind Ktor

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,29 @@
 package com.androiddevs
 
-import io.ktor.application.Application
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    //  All server configurations (features) goes into this file
+
+    //  Intercept responses with more information
+    install(DefaultHeaders)
+    //  Will log all requests to the server and the responses
+    install(CallLogging)
+    //  Essential feature for the server - to make it rest API
+    install(Routing)
+    //  Make sure what content the server responds with
+    install(ContentNegotiation){
+        //  Configuring content negotiation - answer and expect JSON
+        gson {
+            setPrettyPrinting()
+        }
+    }
 }
 
